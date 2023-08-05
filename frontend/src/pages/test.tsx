b02989c4f10c472dea9fd326cd6848aece4d4970
JokerZ75 <deaconhughes07@gmail.com>
2023-08-05 15:23:35 +0100

    Dark theme toggle test

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 const Test = () => {
+    const [theme, setTheme] = React.useState(false);
+
+    React.useEffect(() => {
+        if (theme) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    }, [theme]);
+
     return (
         <div>
             <h1 className="Test">Test Page</h1>
+            <button onClick={() => setTheme(!theme)}>Toggle Theme</button>
         </div>
     );
 };
