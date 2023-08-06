31cb47ef3eb3cbe8eb87b4b83ee55d2494610392
JokerZ75 <deaconhughes07@gmail.com>
2023-08-06 23:13:44 +0100

    Setting out content for home screen

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,11 +3,39 @@ import { Layout } from "../components";
 
 const index = () => {
   return (
-      <div>
+    <>
+      <div id="main-content-container">
         <div id="heading">
           <h1>Dashboard</h1>
         </div>
+        <div id="cards-holder">
+          <div className="card">
+            <div className="card-heading">
+              <h2>Sales Graph</h2>
+            </div>
+            <div className="card-body-graph"></div>
+          </div>
+          <div className="card table">
+            <div className="card-heading">
+              <h2>Recent Orders</h2>
+            </div>
+            <div className="card-body-table"></div>
+          </div>
+          <div className="card">
+            <div className="card-heading">
+              <h2>Upcoming Shipments</h2>
+            </div>
+            <div className="card-body-table"></div>
+          </div>
+          <div id="money-in" className="card">
+            <div className="card-heading">
+              <h2>Total Money In</h2>
+            </div>
+            <div className="card-body"></div>
+          </div>
+        </div>
       </div>
+    </>
   );
 };
 
