0bf9cdae1a22b3352136a3ff65ad51b3fea103c2
Deacon Hughes <deaconhughes07@gmail.com>
2023-08-09 13:32:13 +0100

    Preq for shadcn ui table

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss"
 import "./styles/desktop.scss"
+import "./styles/global.scss"
 import { Layout } from "./components";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
