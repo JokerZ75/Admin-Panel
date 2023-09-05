dbc5627bd765a6e208ac93f9196aa897dc87e3d9
JokerZ75 <deaconhughes07@gmail.com>
2023-09-05 19:48:11 +0100

    Dynamic sales graph

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -7,68 +7,20 @@ import {
   CartesianGrid,
   Tooltip,
   Area,
-  Legend
+  Legend,
 } from "recharts";
 
 interface graphProps {
-  dataS?: any;
+  data?: {
+    name: string;
+    sales: number;
+  }[];
 }
 
-const Graph: FC<graphProps> = ({ dataS }) => {
-  const data = [
-    {
-        name: "Jan",
-        uv: 4000,
-    },
-    {
-        name: "Feb",
-        uv: 3000,
-    },
-    {
-        name: "Mar",
-        uv: 2000,
-    },
-    {
-        name: "Apr",
-        uv: 2780,
-    },
-    {
-        name: "May",
-        uv: 1890,
-    },
-    {
-        name: "Jun",
-        uv: 2390,
-    },
-    {
-        name: "Jul",
-        uv: 3490,
-    },
-    {
-        name: "Aug",
-        uv: 4000,
-    },
-    {
-        name: "Sep",
-        uv: 3000,
-    },
-    {
-        name: "Oct",
-        uv: 2000,
-    },
-    {
-        name: "Nov",
-        uv: 2780,
-    },
-    {
-        name: "Dec",
-        uv: 1890,
-    },
-  ];
-
+const Graph: FC<graphProps> = ({ data }) => {
   return (
     <>
-      <ResponsiveContainer width="100%" height="80%"> 
+      <ResponsiveContainer width="100%" height="80%">
         <AreaChart
           data={data}
           margin={{
@@ -78,20 +30,20 @@ const Graph: FC<graphProps> = ({ dataS }) => {
             bottom: 0,
           }}
         >
-            <Legend verticalAlign="top" height={38} />
-            <defs>
-                <linearGradient id="colorLine" x1={0} y1={0} x2={0} y2={1}>
-                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                </linearGradient>
-            </defs>
+          <Legend verticalAlign="top" height={38} />
+          <defs>
+            <linearGradient id="colorLine" x1={0} y1={0} x2={0} y2={1}>
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
           <XAxis id="XAxis" dataKey="name" color="#ffffff" opacity={1} />
-          <YAxis id="YAxis"/>
+          <YAxis id="YAxis" />
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip />
           <Area
             type="monotone"
-            dataKey="uv"
+            dataKey="sales"
             stroke="#8884d8"
             fillOpacity={1}
             fill="url(#colorLine)"
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,7 +10,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { json } from "stream/consumers";
 import { set } from "react-hook-form";
-import useEffect from 'react';
+import useEffect from "react";
 
 interface Product {
   item: string;
@@ -19,33 +19,73 @@ interface Product {
 }
 
 const index = () => {
-  const { data, isError } = useQuery({
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const { data, isError, error } = useQuery({
     queryKey: ["orders"],
     queryFn: async () => {
       const { data } = await axios.get("http://localhost:8008/orders");
       let products = await data.flatMap((order: RecentOrder) => {
-        return order.products
+        return order.products;
       });
-      let RecentOrders = await data.map((order: RecentOrder) => {
-        if (new Date(order.createdAt) < new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) && new Date(order.createdAt) > new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)) {
-           return order;
-        } else{
-          return null;
-        }
+      let RecentOrders = await data
+        .map((order: RecentOrder) => {
+          if (
+            new Date(order.createdAt) <
+              new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) &&
+            new Date(order.createdAt) >
+              new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)
+          ) {
+            return order;
+          } else {
+            return null;
+          }
+        })
+        .filter((order: RecentOrder) => order !== null);
+      let monthMap = new Map<string, number>();
+      await data.forEach((order: RecentOrder) => {
+        let month = new Date(order.createdAt).getMonth().toString();
+        month = months[parseInt(month)];
 
-      }).filter((order: RecentOrder) => order !== null);
+        if (monthMap.has(month)) {
+          monthMap.set(month, monthMap.get(month)! + 1);
+        } else {
+          monthMap.set(month, 1);
+        }
+      });
+      console.log(monthMap);
+      let graphData: Array<{
+        name: string;
+        sales: number;
+      }> = [];
+      monthMap.forEach((value: number, key: string) => {
+        graphData.push({ name: key, sales: value });
+      });
+
+      setGraphData(graphData);
       setRecentOrders(RecentOrders);
       setProducts(products);
       return data as RecentOrder[];
     },
   });
 
-
-
   const [products, setProducts] = useState<Product[]>([]);
   const [recentOrders, setRecentOrders] = useState<RecentOrder[]>([]);
+  const [graphData, setGraphData] = useState<any[]>([]);
   const [bestSeller, setBestSeller] = useState<string>("");
-  
+
   React.useEffect(() => {
     let quantityTracker = new Map<string, number>();
     products.forEach((product: Product) => {
@@ -65,10 +105,13 @@ const index = () => {
         max = value;
         bestSeller = key;
       }
-    }
-    );
+    });
     setBestSeller(bestSeller);
   }, [products]);
+
+  React.useEffect(() => {
+    console.log(error);
+  }, [data]);
 
   return (
     <>
@@ -93,14 +136,12 @@ const index = () => {
             )}
           </Card>
           <Card cardClass="smaller-card" title="Best Seller">
-            {(data && (
-              <p className="card-text-large">
-                {bestSeller}
-              </p>
-            )) || <p>Loading...</p>}
+            {(data && <p className="card-text-large">{bestSeller}</p>) || (
+              <p>Loading...</p>
+            )}
           </Card>
           <Card bodyID="Graph" id="sales-graph" title="Sales Graph">
-            <Graph />
+            <Graph data={graphData} />
           </Card>
           <Card title="Recent Orders">
             {(data && (
